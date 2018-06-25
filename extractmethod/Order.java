ce2e2b7f1f020aa1014779654060cf9abc8ec5d0
ore-ling <31461936+ore-lingyu@users.noreply.github.com>
2018-06-25 17:58:02 +0800

    Add files via upload

---
+++ extractmethod/Order.java
@@ -0,0 +1,28 @@
+package extractmethod;
+
+import java.util.Enumeration;
+
+public class Order {
+	private double amount;
+	private double basePrice;
+	
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public double getBasePrice() {
+		return basePrice;
+	}
+
+	public void setBasePrice(double basePrice) {
+		this.basePrice = basePrice;
+	}
+	
+	
+
+	
+}
