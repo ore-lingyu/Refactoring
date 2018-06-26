70bf4bef79b63ad0d178f103259549e68c04403b
ore-ling <31461936+ore-lingyu@users.noreply.github.com>
2018-06-26 18:34:51 +0800

    Empty Class
    for test

---
+++ ReOrgnizeMethod/Order.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Enumeration;
+
+public class Order {
+	private Double amount;
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	
+}
