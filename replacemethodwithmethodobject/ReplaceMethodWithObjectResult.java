61f333f2f104a13904ad961601567379cbc28b22
ore-ling <31461936+ore-lingyu@users.noreply.github.com>
2018-06-26 18:38:14 +0800

    Delete ReplaceMethodWithObjectResult.java

--- replacemethodwithmethodobject/ReplaceMethodWithObjectResult.java
+++
@@ -1,7 +0,0 @@
-package replacemethodwithmethodobject;
-public class ReplaceMethodWithObjectResult {
-	
-	int gamma(int inputVal,int quantity, int yearToDate) {
-		return new Gamma(this,inputVal,quantity,yearToDate).compute();
-	}
-}
