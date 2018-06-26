65a95459e9c9ce88022d0e5d57057a8703dc024f
ore-ling <31461936+ore-lingyu@users.noreply.github.com>
2018-06-26 18:25:30 +0800

    Remove Assignments to Parameters
    移除对参数的赋值
    代码对一个参数进行赋值
    以一个临时变量取代该参数的位置

---
+++ ReOrgnizeMethod/removeassignmentstoparameters/RemoveAssignToParamResult.java
@@ -0,0 +1,12 @@
+package removeassignmentstoparameters;
+
+public class RemoveAssignToParamResult {
+	
+	int discount(int inputVal, int quantity, int yearToDate) {
+		int result = inputVal;
+		if(inputVal > 50) result -= 2;
+		if(quantity > 100) result -= 1;
+		if(yearToDate > 10000) result -= 4;
+		return result;
+	}
+}
---
+++ ReOrgnizeMethod/removeassignmentstoparameters/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 移除对参数的赋值
+ * 代码对一个参数进行赋值
+ * 以一个临时变量取代该参数的位置
+ */
+/**
+ * @author lingyu
+ *
+ */
+package removeassignmentstoparameters;
