c82e7c0b6baee89ae5a0d986641d8ceac12cc106
ore-ling <31461936+ore-lingyu@users.noreply.github.com>
2018-07-04 14:20:29 +0800

    Duplicate Observed Data
     * 复制“被监视数据”
     * 你有一些领域数据置身于GUI控件之中，而领域函数需要访问这些数据
     * 将该数据复制到一个领域对象中，建立一个Observer模式，
     * 用以同步领域对象和GUI对象内的重复数据

---
+++ ReOrgnizeData/duplicateobserveddata/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 复制“被监视数据”
+ * 你有一些领域数据置身于GUI控件之中，而领域函数需要访问这些数据
+ * 将该数据复制到一个领域对象中，建立一个Observer模式，
+ * 用以同步领域对象和GUI对象内的重复数据
+ */
+/**
+ * @author lingyu
+ *
+ */
+package duplicateobserveddata;
