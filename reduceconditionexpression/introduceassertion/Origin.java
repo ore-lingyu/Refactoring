fc18c8c076188ffff4d25801d0e26c78763d1be0
ore-ling <18633837894@163.com>
2018-07-12 16:52:44 +0800

    Introduce Assertion
     * 引入断言
     * 某一段代码需要对程序状态做出某种假设
     * 以断言明确表现这种假设

---
+++ reduceconditionexpression/introduceassertion/Origin.java
@@ -0,0 +1,5 @@
+package introduceassertion;
+
+public class Origin {
+	
+}
---
+++ reduceconditionexpression/introduceassertion/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 引入断言
+ * 某一段代码需要对程序状态做出某种假设
+ * 以断言明确表现这种假设
+ */
+/**
+ * @author lingyu
+ *
+ */
+package introduceassertion;
